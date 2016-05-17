078d5f4a393ce517be6cb90308b3f2b052effdcb
fionamatters <fionalmatters@gmail.com>
2016-05-17 21:14:55 +0100

    Continued max flow

--- Section1/MaxFlow.py
+++ Section1/MaxFlow.py
@@ -1,7 +1,29 @@
+import collections
+Edge = collections.namedtuple("Edge", "node capacity")
 class MaxFlow:
-    def __init__(self,node_count, source, sink, edges):
+    def __init__(self, node_count, source, sink, capacity):
+        self.node_count = node_count
+        self.source = source
+        self.sink = sink
+        self.capacity = capacity
+        self.flow = [[0]*self.node_count for _ in range(self.node_count)]
+
+    def calculate(self):
+        residual = self.get_residual()
         a = 1
 
-def get_adj_matrix(node_count, edges):
-    a=1
-    return 1
+    def get_residual(self):
+        residual = collections.defaultdict(list)
+        for (n1, n2), capacity in self.capacity.items():
+            if (capacity - self.flow[n1][n2]) > 0:
+                residual[n1].append(Edge(n2,capacity - self.flow[n1][n2]))
+            if self.flow[n1][n2]>0:
+                residual[n2].append(Edge(n1,self.flow[n1][n2]))
+        return residual
+
+
+def get_adj_dict(edges):
+    adj_dict={}
+    for n1, n2, capacity in edges:
+        adj_dict[n1,n2]= capacity
+    return adj_dict
--- Tests/Section1/test_MaxFlow.py
+++ Tests/Section1/test_MaxFlow.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 import sys
-from Section1.MaxFlow import MaxFlow, get_adj_matrix
+from Section1.MaxFlow import MaxFlow, get_adj_dict
 from collections import namedtuple
 
 Edge = namedtuple('Edge','n1 n2 cap')
@@ -8,10 +8,11 @@ class TestMaxFlow(TestCase):
     def test_initial_case(self):
         adj_list = [Edge(0,1,3),Edge(0,2,3),Edge(1,3,3),Edge(1,2,2),Edge(2,4,2),Edge(3,5,2),Edge(3,4,4),Edge(4,5,3)]
         node_count = 6
-        adj_matrix = get_adj_matrix(node_count, adj_list)
+        adj_matrix = get_adj_dict(adj_list)
         source = 0
         sink = 5
-        #test_subject =
+        test_subject = MaxFlow(node_count, source, sink, adj_matrix)
+        test_subject.calculate()
 
         output_string = '''6
 6
