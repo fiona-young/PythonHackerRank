4aab023bbf8689b954368327cd8bd235d7ec4d45
fionamatters <fionalmatters@gmail.com>
2016-05-18 22:56:11 +0100

    Initial max flow

--- Section1/MaxFlow.py
+++ Section1/MaxFlow.py
@@ -1,29 +1,86 @@
 import collections
+
 Edge = collections.namedtuple("Edge", "node capacity")
+
+
 class MaxFlow:
-    def __init__(self, node_count, source, sink, capacity):
+    def __init__(self, node_count, source, sink, capacity: dict):
         self.node_count = node_count
         self.source = source
         self.sink = sink
         self.capacity = capacity
-        self.flow = [[0]*self.node_count for _ in range(self.node_count)]
+        self.flow = [[0] * self.node_count for _ in range(self.node_count)]
+        self.flow2 = {keys:0 for keys in self.capacity.keys()}
 
     def calculate(self):
-        residual = self.get_residual()
-        a = 1
+        while True:
+            residual = self.get_residual()
+            shortest_path, flow = self.breadth_first_search(residual)
+            if len(shortest_path) == 0:
+                break
+            self.add_flow(shortest_path, flow)
+        return sum([flow for (n1, n2), flow in self.flow2.items() if n1 == self.source])
+
+    def add_flow(self, shortest_path, flow):
+        for i in range(len(shortest_path) - 1):
+            from_node = shortest_path[i]
+            to_node = shortest_path[i + 1]
+            if (from_node, to_node) in self.flow2:
+                self.flow2[from_node,to_node] += flow
+            else:
+                self.flow2[to_node,from_node] -= flow
 
     def get_residual(self):
-        residual = collections.defaultdict(list)
+        residual = {i: [] for i in range(self.node_count)}
+
         for (n1, n2), capacity in self.capacity.items():
-            if (capacity - self.flow[n1][n2]) > 0:
-                residual[n1].append(Edge(n2,capacity - self.flow[n1][n2]))
-            if self.flow[n1][n2]>0:
-                residual[n2].append(Edge(n1,self.flow[n1][n2]))
+            if (capacity - self.flow2[n1,n2]) > 0:
+                residual[n1].append(Edge(n2, capacity - self.flow2[n1,n2]))
+            if self.flow2[n1,n2] > 0:
+                residual[n2].append(Edge(n1, self.flow2[n1,n2]))
         return residual
 
+    def breadth_first_search(self, graph: dict):
+        node_dict = {node_id: Node(node_id) for node_id in graph.keys()}
+        queue = collections.deque()
+        queue.append(node_dict[self.source])
+        node_dict[self.source].length = 0
+        while len(queue) > 0:
+            current_node = queue.pop()
+            current_node.visited = True
+            for adj_node_id, capacity in graph[current_node.node_id]:
+                adj_node = node_dict[adj_node_id]
+                if not adj_node.visited:
+                    adj_node.update(current_node.node_id, capacity, current_node.length + 1)
+                    queue.append(adj_node)
+                    if adj_node_id == self.sink:
+                        queue.clear()
+                        break
+        my_node = node_dict[self.sink]
+        if not my_node.visited:
+            return [], None
+        weight = my_node.weight
+        path = []
+        while my_node.parent is not None:
+            path.append(my_node.node_id)
+            weight = min(weight, my_node.weight)
+            my_node = node_dict[my_node.parent]
+        path.append(self.source)
+        path.reverse()
+        return path, weight
 
-def get_adj_dict(edges):
-    adj_dict={}
-    for n1, n2, capacity in edges:
-        adj_dict[n1,n2]= capacity
-    return adj_dict
+
+class Node:
+    def __init__(self, node_id):
+        self.node_id = node_id
+        self.length = None
+        self.weight = None
+        self.visited = False
+        self.parent = None
+
+    def update(self, parent_id, weight, length):
+        self.parent = parent_id
+        self.weight = weight
+        self.length = length
+        self.visited = True
+
--- Tests/Section1/test_MaxFlow.py
+++ Tests/Section1/test_MaxFlow.py
@@ -1,23 +1,15 @@
 from unittest import TestCase
 import sys
-from Section1.MaxFlow import MaxFlow, get_adj_dict
+from Section1.MaxFlow import MaxFlow
 from collections import namedtuple
 
 Edge = namedtuple('Edge','n1 n2 cap')
 class TestMaxFlow(TestCase):
     def test_initial_case(self):
-        adj_list = [Edge(0,1,3),Edge(0,2,3),Edge(1,3,3),Edge(1,2,2),Edge(2,4,2),Edge(3,5,2),Edge(3,4,4),Edge(4,5,3)]
+        adj_dict = {(0, 1): 3, (0, 2): 3, (1, 3): 3, (1, 2): 2, (2, 4): 2, (3, 5): 2, (3, 4): 4, (4, 5): 3}
         node_count = 6
-        adj_matrix = get_adj_dict(adj_list)
         source = 0
         sink = 5
-        test_subject = MaxFlow(node_count, source, sink, adj_matrix)
-        test_subject.calculate()
-
-        output_string = '''6
-6
-8
-32
-'''
-        self.assertEqual(output_string, sys.stdout.getvalue())
+        test_subject = MaxFlow(node_count, source, sink, adj_dict)
+        self.assertEqual(5,test_subject.calculate())
 
