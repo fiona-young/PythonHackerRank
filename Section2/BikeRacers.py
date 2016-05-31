e8417d2b005d3acecfe5d06284d4df0248552733
fionamatters <fionalmatters@gmail.com>
2016-05-31 08:32:37 +0100

    Update: I have a plan. There are graphs

---
+++ Section2/BikeRacers.py
@@ -0,0 +1,82 @@
+import collections
+import math
+
+Coord = collections.namedtuple('Coord', 'x y')
+Edge = collections.namedtuple('Edge', 'node distance')
+
+
+def read_input():
+    riders, bikes, max_participants = get_int_list(input())
+    rider_list, bike_list = {}, {}
+    for i in range(riders):
+        rider_list[Coord(*get_float_list(input()))] = i
+    for i in range(riders):
+        bike_list[Coord(*get_float_list(input()))] = i
+    return max_participants, rider_list, bike_list
+
+
+class BikeRacer:
+    def __init__(self):
+        self.max_participants, self.riders_list, self.bike_list = read_input()
+#        self.graph = collections.defaultdict(list)
+#        self.dist = {}
+#        self.dist_matrix = []
+        self.tuples_bike = collections.defaultdict(list)
+        self.tuples_rider = collections.defaultdict(list)
+        self.distances = []
+
+    def calculate_distances(self):
+        for i_rider, rider in enumerate(self.riders_list):
+            dist = []
+            for i_bike, bike in enumerate(self.bike_list):
+                distance = math.sqrt((rider.x - bike.x) ** 2 + (rider.y - bike.y) ** 2)
+                dist.append(distance)
+                self.tuples_bike[i_bike].append((distance, i_rider))
+                self.tuples_rider[i_rider].append((distance, i_bike))
+                self.distances.append((distance, i_rider, i_bike))
+#            self.dist_matrix.append(dist)
+        for bike_list in self.tuples_bike.values():
+            bike_list.sort()
+        for rider_list in self.tuples_rider.values():
+            rider_list.sort()
+        self.distances.sort()
+        rider_d=collections.defaultdict(set)
+        bike_d =collections.defaultdict(set)
+        pair=set()
+        i_pointer = 0
+        distance = 0
+        while len(bike_d) < self.max_participants or len(rider_d) < self.max_participants or get_independent_set_count(bike_d)<self.max_participants:
+            distance,rider, bike = self.distances[i_pointer]
+            pair.add((rider,bike))
+            bike_d[bike].add(rider)
+            rider_d[rider].add(bike)
+            i_pointer += 1
+        return distance
+
+    def calculate(self):
+        return round(self.calculate_distances() ** 2)
+
+def get_independent_set_count(bike_d):
+    count = 0
+    already_used = set()
+    for rider_set in bike_d.values():
+        if len(rider_set.difference(already_used)) > 0:
+            count += 1
+        already_used.update(rider_set)
+    return count
+
+def main():
+    my_obj = BikeRacer()
+    print(my_obj.calculate())
+
+
+def get_int_list(in_str):
+    return [int(i) for i in in_str.strip().split()]
+
+
+def get_float_list(in_str):
+    return [float(i) for i in in_str.strip().split()]
+
+
+if __name__ == "__main__":
+    main()
--- Section2/ScratchPad.py
+++ Section2/ScratchPad.py
@@ -1,9 +1,10 @@
 import array
 
+
 class Fenwick:
     def __init__(self, size):
         self.size = size
-        self.array = array.array('l', [0] * (size+1))
+        self.array = array.array('l', [0] * (size + 1))
         self.bare_array = array.array('l', [0] * size)
 
     def update(self, key, value):
@@ -11,28 +12,78 @@ class Fenwick:
         difference = value - last_value
         self.bare_array[key] = value
         tree_key = key + 1
-        while tree_key < self.size+1:
+        while tree_key < self.size + 1:
             self.array[tree_key] += difference
             tree_key += tree_key & -tree_key
 
     def query(self, key):
-        tree_key = min(key + 1,self.size)
+        tree_key = min(key + 1, self.size)
         sum = 0
         while tree_key > 0:
             sum += self.array[tree_key]
             tree_key -= tree_key & -tree_key
         return sum
 
+
 class FenwickRangeFinder:
     def __init__(self, size):
         self.fenwick = Fenwick(size)
 
     def add(self, node_id):
-        self.fenwick.update( node_id, 1)
+        self.fenwick.update(node_id, 1)
 
     def remove(self, node_id):
-        self.fenwick.update( node_id, 0)
+        self.fenwick.update(node_id, 0)
 
     def range_count(self, min_val, max_val):
-        count_fen = self.fenwick.query(max_val)-self.fenwick.query(min_val-1)
+        count_fen = self.fenwick.query(max_val) - self.fenwick.query(min_val - 1)
         return count_fen
+
+class PairGenerator:
+    def __init__(self, pair_list, used1, used2):
+        self.pair_list = pair_list
+        self.used1 = used1
+        self.used2 = used2
+
+    def __repr__(self):
+        return str(self.pair_list)
+
+    def get_next(self, item1, item2):
+        my_pair = PairGenerator(self.pair_list.append((item1, item2)),self.used1.union({item1}),self.used2.union({item2}))
+        return my_pair
+
+class Pairs:
+    def __init__(self, list1 :list, list2: list):
+        self.list1 = list1
+        self.list2 = list2
+        self.combination = [[PairGenerator([(item1, item2)],{item1},{item2}) for item1 in list1 for item2 in list2]]
+
+
+    def get_pairs(self, length):
+        calculated_length =  len(self.combination)
+        my_list = [[]]
+        for item1 in self.list1:
+            for item2 in self.list2:
+                my_list[0].append('%s:%s'%(item1,item2))
+        my_list.append([])
+        for i in range(1,2):
+            for last_item in my_list[i-1]:
+                for item1 in self.list1:
+                    for item2 in self.list2:
+                        my_list[i].append('%s|%s:%s'%(last_item,item1,item2))
+
+        if length <= calculated_length:
+            return self.combination[length-1]
+        else:
+            for i_length in range(calculated_length,length):
+                self.combination.append([])
+                for last_list in self.combination[i_length-1]:
+                    remaining_set1 = self.set1.difference(last_set.used1)
+                    for item1 in remaining_set1:
+                        for item2 in self.set2.difference(last_set.used2):
+                            self.combination[length-1].append(last_set.get_next(item1, item2))
+                    print(last_set)
+                print(i_length)
+        print(self.set1, self.set2, length)
+
+
